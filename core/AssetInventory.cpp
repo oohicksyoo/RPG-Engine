67457e20bcca16ed66ed2f227c07f62597905bce
Alex Hicks <alexhicks@live.ca>
2020-12-14 21:34:24 -0500

    Added return defaults to avoid warnings

--- core/AssetInventory.cpp
+++ core/AssetInventory.cpp
@@ -7,6 +7,7 @@
 std::string RPG::Assets::ResolvePipelinePath(const RPG::Assets::Pipeline &pipeline) {
 	switch (pipeline) {
 		case RPG::Assets::Pipeline::Default:
+		default:
 			return "default";
 	}
 }
@@ -17,6 +18,8 @@ std::string RPG::Assets::ResolveStaticMeshPath(const RPG::Assets::StaticMesh &st
 			return "assets/models/crate.obj";
 		case RPG::Assets::StaticMesh::Torus:
 			return "assets/models/torus.obj";
+		default:
+			return "";
 	}
 }
 
@@ -26,5 +29,7 @@ std::string RPG::Assets::ResolveTexturePath(const RPG::Assets::Texture &texture)
 			return "assets/textures/crate.png";
 		case RPG::Assets::Texture::RedCrossHatch:
 			return "assets/textures/red_cross_hatch.png";
+		default:
+			return "";
 	}
 }
