1c926159594311d5173590759032a62d2ce6d1c9
Alex Hicks <alexhicks@live.ca>
2021-01-02 00:05:13 -0500

    MeshComponent

---
+++ core/components/MeshComponent.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Alex on 1/1/2021.
+//
+
+#include "MeshComponent.hpp"
+
+using RPG::MeshComponent;
+
+struct MeshComponent::Internal {
+	std::shared_ptr<RPG::Property> mesh;
+	std::shared_ptr<RPG::Property> texture;
+
+	Internal(RPG::Assets::StaticMesh mesh, RPG::Assets::Texture texture)
+	: mesh(std::make_unique<RPG::Property>(mesh, "Mesh", "RPG::Assets::StaticMesh")),
+	  texture(std::make_unique<RPG::Property>(texture, "Texture", "RPG::Assets::Texture")) {}
+};
+
+MeshComponent::MeshComponent(RPG::Assets::StaticMesh mesh, RPG::Assets::Texture texture) : internal(MakeInternalPointer<Internal>(mesh, texture)) {}
+
+void MeshComponent::Awake() {
+
+}
+
+void MeshComponent::Start() {
+
+}
+
+void MeshComponent::Update() {
+
+}
+
+std::vector<std::shared_ptr<RPG::Property>> MeshComponent::GetProperties() {
+	std::vector<std::shared_ptr<RPG::Property>> list = {};
+	list.push_back(internal->mesh);
+	list.push_back(internal->texture);
+
+	return list;
+}
---
+++ core/components/MeshComponent.hpp
@@ -0,0 +1,27 @@
+//
+// Created by Alex on 1/1/2021.
+//
+
+#pragma once
+
+#include "../IComponent.hpp"
+#include "../InternalPointer.hpp"
+#include "../AssetInventory.hpp"
+
+namespace RPG {
+	struct MeshComponent : public IComponent {
+		public:
+			MeshComponent(RPG::Assets::StaticMesh mesh, RPG::Assets::Texture texture);
+			void Awake() override;
+			void Start() override;
+			void Update() override;
+			std::string Name() { return "MeshComponent";};
+			std::vector<std::shared_ptr<RPG::Property>> GetProperties() override;
+
+		private:
+			struct Internal;
+			RPG::InternalPointer<Internal> internal;
+	};
+}
+
+
