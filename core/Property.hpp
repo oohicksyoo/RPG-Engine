518cdc73945c04ffac85cfc83d914b974c269714
Alex Hicks <alexhicks@live.ca>
2021-01-26 16:53:30 -0500

    Added Drag and Drop support to properties

--- core/Property.hpp
+++ core/Property.hpp
@@ -12,10 +12,12 @@
 namespace RPG {
 	struct Property {
 		public:
-			Property(std::any property, std::string propertyName, std::string propertyType) {
+			Property(std::any property, std::string propertyName, std::string propertyType, bool propertyAllowsDragAndDrop = false, std::string propertyDragAndDropTag = "") {
 				name = propertyName;
 				type = propertyType;
 				guid = RPG::Guid::GenerateGuid();
+				allowsDragAndDrop = propertyAllowsDragAndDrop;
+				dragAndDropTag = propertyDragAndDropTag;
 				SetProperty(property);
 			};
 			void SetProperty(std::any property);
@@ -28,12 +30,20 @@ namespace RPG {
 			std::string GetEditorName() {
 				return GetName() + "##" + GetGuid();
 			}
+			bool AllowsDragAndDrop() {
+				return allowsDragAndDrop;
+			}
+			std::string DragAndDropTag() {
+				return dragAndDropTag;
+			}
 
 		private:
 			std::any prop;
 			std::string name;
 			std::string type;
 			std::string guid;
+			bool allowsDragAndDrop;
+			std::string dragAndDropTag;
 	};
 }
 
--- core/components/MeshComponent.cpp
+++ core/components/MeshComponent.cpp
@@ -14,8 +14,8 @@ struct MeshComponent::Internal {
 
 	Internal(std::string mesh, std::string texture, std::string guid)
 	: guid(guid),
-	  mesh(std::make_unique<RPG::Property>(mesh, "Mesh", "std::string")),
-	  texture(std::make_unique<RPG::Property>(texture, "Texture", "std::string")) {}
+	  mesh(std::make_unique<RPG::Property>(mesh, "Mesh", "std::string", true, "Model")),
+	  texture(std::make_unique<RPG::Property>(texture, "Texture", "std::string", true, "Texture")) {}
 };
 
 MeshComponent::MeshComponent(std::string mesh, std::string texture, std::string guid) : internal(MakeInternalPointer<Internal>(mesh, texture, guid)) {}
